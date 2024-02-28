acb0fcb64aafb5819138d018afff9410edda0353
Edmund Miller <git@edmundmiller.dev>
2024-02-28 10:41:05 -0600

    fix: Maybe this will work?
    https://github.com/actions/toolkit/tree/main/packages/tool-cache
    https://github.com/google-github-actions/setup-gcloud

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,8 @@
 /***/ 9476:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
+const fs = __nccwpck_require__(7147);
+const os = __nccwpck_require__(2037);
 const path = __nccwpck_require__(1017);
 const core = __nccwpck_require__(2933);
 const tc = __nccwpck_require__(5707);
@@ -15,17 +17,39 @@ async function setup() {
         const version = core.getInput("version");
 
         // Download the specific version of the tool, e.g. as a tarball/zipball
-        const download = getDownloadObject(version);
-        const pathToTarball = await tc.downloadTool(download.url);
+        const toolPath = tc.find("nf-test", version);
+        if (toolPath !== "") {
+            core.addPath(path.join(toolPath, "bin"));
+        } else {
+            core.debug(
+                `no version of nf-test matching "${version}" is installed`,
+            );
+            const download = getDownloadObject(version);
+            const pathToTarball = await tc.downloadTool(download.url);
 
-        // Extract the tarball/zipball onto host runner
-        const extract = download.url.endsWith(".zip")
-            ? tc.extractZip
-            : tc.extractTar;
-        const pathToCLI = await extract(pathToTarball);
+            // Extract the tarball/zipball onto host runner
+            const extract = download.url.endsWith(".zip")
+                ? tc.extractZip
+                : tc.extractTar;
+            const pathToCLI = await extract(pathToTarball);
 
-        // Expose the tool by adding it to the PATH
-        core.addPath(path.join(pathToCLI, download.binPath));
+            // Expose the tool by adding it to the PATH
+            tc.cacheFile(
+                path.join(pathToCLI, download.binPath),
+                "nf-test",
+                version,
+            );
+            core.addPath(path.join(pathToCLI, download.binPath));
+
+            // Move the jar to ~/.nf-test/nf-test.jar
+            fs.mkdirSync(path.join(os.homedir(), ".nf-test"));
+            jar_final_path = fs.renameSync(
+                path.join(pathToCLI, download.jarPath),
+                path.join(os.homedir(), ".nf-test", "nf-test.jar"),
+            );
+            // Cache the jar
+            tc.cacheFile(jar_final_path, "nf-test.jar", version);
+        }
     } catch (e) {
         core.setFailed(e);
     }
@@ -50,11 +74,13 @@ function getDownloadObject(version) {
     const platform = os.platform();
     const filename = `nf-test-${version}`;
     const extension = "tar.gz";
-    const binPath = platform === "win32" ? "bin" : path.join(filename, "bin");
+    const binPath = path.join(filename, "nf-test");
+    const jarPath = path.join(filename, "nf-test.jar");
     const url = `https://github.com/askimed/nf-test/releases/download/v${version}/${filename}.${extension}`;
     return {
         url,
         binPath,
+        jarPath,
     };
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const os = require("os");
 const path = require("path");
 const core = require("@actions/core");
 const tc = require("@actions/tool-cache");
@@ -9,17 +11,39 @@ async function setup() {
         const version = core.getInput("version");
 
         // Download the specific version of the tool, e.g. as a tarball/zipball
-        const download = getDownloadObject(version);
-        const pathToTarball = await tc.downloadTool(download.url);
+        const toolPath = tc.find("nf-test", version);
+        if (toolPath !== "") {
+            core.addPath(path.join(toolPath, "bin"));
+        } else {
+            core.debug(
+                `no version of nf-test matching "${version}" is installed`,
+            );
+            const download = getDownloadObject(version);
+            const pathToTarball = await tc.downloadTool(download.url);
 
-        // Extract the tarball/zipball onto host runner
-        const extract = download.url.endsWith(".zip")
-            ? tc.extractZip
-            : tc.extractTar;
-        const pathToCLI = await extract(pathToTarball);
+            // Extract the tarball/zipball onto host runner
+            const extract = download.url.endsWith(".zip")
+                ? tc.extractZip
+                : tc.extractTar;
+            const pathToCLI = await extract(pathToTarball);
 
-        // Expose the tool by adding it to the PATH
-        core.addPath(path.join(pathToCLI, download.binPath));
+            // Expose the tool by adding it to the PATH
+            tc.cacheFile(
+                path.join(pathToCLI, download.binPath),
+                "nf-test",
+                version,
+            );
+            core.addPath(path.join(pathToCLI, download.binPath));
+
+            // Move the jar to ~/.nf-test/nf-test.jar
+            fs.mkdirSync(path.join(os.homedir(), ".nf-test"));
+            jar_final_path = fs.renameSync(
+                path.join(pathToCLI, download.jarPath),
+                path.join(os.homedir(), ".nf-test", "nf-test.jar"),
+            );
+            // Cache the jar
+            tc.cacheFile(jar_final_path, "nf-test.jar", version);
+        }
     } catch (e) {
         core.setFailed(e);
     }
