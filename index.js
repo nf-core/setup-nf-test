08e4f6d4e6f5fad1225946025ad3179537500b0d
Edmund Miller <git@edmundmiller.dev>
2024-02-28 09:02:50 -0600

    feat: Copy example from example-setup-gh
    https://github.com/github-developer/example-setup-gh

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+const path = require("path");
 const core = require("@actions/core");
 const tc = require("@actions/tool-cache");
+const { getDownloadObject } = require("./lib/utils");
 
 async function setup() {
-    // Get version of tool to be installed
-    const version = core.getInput("version");
+    try {
+        // Get version of tool to be installed
+        const version = core.getInput("version");
 
-    // Download the specific version of the tool, e.g. as a tarball
-    const pathToTarball = await tc.downloadTool(getDownloadURL());
+        // Download the specific version of the tool, e.g. as a tarball/zipball
+        const download = getDownloadObject(version);
+        const pathToTarball = await tc.downloadTool(download.url);
 
-    // Extract the tarball onto the runner
-    const pathToCLI = await tc.extractTar(pathToTarball);
+        // Extract the tarball/zipball onto host runner
+        const extract = download.url.endsWith(".zip")
+            ? tc.extractZip
+            : tc.extractTar;
+        const pathToCLI = await extract(pathToTarball);
 
-    // Expose the tool by adding it to the PATH
-    core.addPath(pathToCLI);
+        // Expose the tool by adding it to the PATH
+        core.addPath(path.join(pathToCLI, download.binPath));
+    } catch (e) {
+        core.setFailed(e);
+    }
 }
 
 module.exports = setup;
+
+if (require.main === module) {
+    setup();
+}
