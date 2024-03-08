5f3f437e6782346300f217756ff9788bcc6e799c
mashehu <mashehu3@gmail.com>
2024-03-08 10:19:48 +0100

    copy changes over from dist/index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ async function setup() {
     const toolPath = tc.find("nf-test", version)
     const jarPath = tc.find("nf-test.jar", version)
     if (toolPath !== "") {
+      core.debug(`nf-test found in cache ${toolPath}`)
       core.addPath(path.join(toolPath, "bin"))
     } else {
       core.debug(`no version of nf-test matching "${version}" is installed`)
@@ -26,24 +27,45 @@ async function setup() {
         : tc.extractTar
       const pathToCLI = await extract(pathToTarball)
 
+      core.debug(`Path to CLI: ${pathToCLI}`)
+      core.debug(`Bin path: ${download.binPath}`)
+
+      // Check if the file exists and is executable
+      if (fs.existsSync(path.join(pathToCLI, download.binPath))) {
+        core.debug("nf-test exists")
+        try {
+          fs.accessSync(
+            path.join(pathToCLI, download.binPath),
+            fs.constants.X_OK
+          )
+          core.debug("nf-test is executable")
+        } catch (err) {
+          core.debug("nf-test is not executable")
+        }
+      } else {
+        core.debug("nf-test does not exist")
+      }
+
       core.debug("Expose the tool by adding it to the PATH")
-      await tc.cacheFile(
+      const cachedPath = await tc.cacheFile(
         path.join(pathToCLI, download.binPath),
         "nf-test",
         "nf-test",
         version
       )
       core.debug("Add nf-test to path")
-      core.addPath(path.join(pathToCLI, download.binPath))
+      core.addPath(path.join(cachedPath))
 
       core.debug("Make ~/.nf-test")
       fs.mkdirSync(path.join(os.homedir(), ".nf-test"))
       core.debug("Move the jar to ~/.nf-test/nf-test.jar")
-      jar_final_path = path.join(os.homedir(), ".nf-test", "nf-test.jar")
-      fs.renameSync(path.join(pathToCLI, "nf-test.jar"), jar_final_path)
+      const jarFinalPath = path.join(os.homedir(), ".nf-test", "nf-test.jar")
+      fs.renameSync(path.join(pathToCLI, "nf-test.jar"), jarFinalPath)
       core.debug("Cache the jar")
       core.debug("Version:")
-      await tc.cacheFile(jar_final_path, "nf-test.jar", "nf-test.jar", version)
+      await tc.cacheFile(jarFinalPath, "nf-test.jar", "nf-test.jar", version)
+
+      core.debug("current Path:" + process.env.PATH)
     }
   } catch (e) {
     core.setFailed(e)
